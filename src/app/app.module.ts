1ab93764e9af3759b5730b047b7bbf2deb400d1e
MrZWH <1875290692@qq.com>
2018-11-20 18:09:47 +0800

    使用 angular-cli 搭建项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // 只能传入指令、组件和管道
   declarations: [
     AppComponent
   ],
+  // 其它需要的模块
   imports: [
     BrowserModule
   ],
   providers: [],
+  // 主程序
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+// 用来关闭 angular 的开发者模式
 import { enableProdMode } from '@angular/core';
+// 告诉 angular 使用哪个模块来启动整个应用
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+// 导入环境配置
 import { environment } from './environments/environment';
 
 if (environment.production) {
